1005fdcfe1541cb90ea2b76d50be4097919cea9d
pio387 <pio387@gmail.com>
2013-11-04 15:54:18 +0100

    Update irq.h
    Added pointers to IDT
    TODO:
    comments, interrputs

--- irq.h
+++ irq.h
@@ -1,8 +1,18 @@
 struct idt_entry_struct
 {
-	u16int base_low; //first 16 bits for 
-	u16int kss;
-	u8int always0;
-	u8int flags;
-	u16int base_high
-	} __attribute__((packed))
+	u16int base_low; /* first 16 bits for adress to jump, where the interrupts fire */ 
+	u16int kss; /* Kernel segment selector */
+	u8int always0; /* Always zero */
+	u8int flags; /* interrupt flag, more in documentation */
+	u16int base_high; /* adress where to jump to */
+	} __attribute__((packed)) 
+typedef struct idt_entry_struct idt_entry_t;
+
+struct idt_pointer
+{
+        u16int limit;
+        u32int base;
+}__attribute((packed))
+
+
+extern void int_1;
